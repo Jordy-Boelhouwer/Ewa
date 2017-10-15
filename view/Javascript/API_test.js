81d6e000505a5bf2333d159135ac91c60e83fb06
Leonard Bos <leonard442@hotmail.com>
2017-10-15 17:35:47 +0200

    POST function bijna af

--- view/Javascript/API_test.js
+++ view/Javascript/API_test.js
@@ -1,40 +1,69 @@
-function init(){
+$( document ).ready(function() {
+console.log("ads");
+
     var posts;
     let url="http://localhost:8080/infosupportAPI/services/rest/profiles";
-    let promise = fetch(url);
 
-    promise.then(function(resp){
-        return resp.json();
-    }).then(function(json){
-        posts=json;
-        display();
+    //let promise = fetch(url);
+
+    // promise.then(function(resp){
+    //     return resp.json();
+    // }).then(function(json){
+    //     posts=json;
+    //     display();
+    // });
+    //
+    // promise.catch(function(err){console.log("jammer")});
+
+    $.ajax({
+      type: 'GET',
+      url: url,
+      dataType:'json',
+      success: function(data){
+          $.each(data, function(i, post){
+            for (var i = 0; i < post.posts.length; i++) {
+              $("#grid").append( `<div class="mdl-card mdl-cell mdl-cell--12-col mdl-shadow--2dp">
+                      <div class="mdl-card__title">
+                        <h2 class="mdl-card__title-text">`+ post.posts[i].title +`</h2>
+                      </div>
+                          <div class="mdl-card__supporting-text">
+                            <p>`+post.posts[i].content+`</p>
+                          </div>
+                          <div class="mdl-card__actions mdl-card--border">
+                            <h6>Written by: `+ post.first_name + " " + post.last_name +`</h6>
+                          </div>
+                    </div>`);
+            };
+          });
+        },
+        error: function(){
+          alert('error loading messages');
+        }
     });
 
-    promise.catch(function(err){console.log("jammer")});
+    $('#addPost').on('click', function(){
 
+      var newtitle = $('#newPostTitle');
+      var newpost =  $('#newPost');
 
-    function display(){
-      // var profileNameContainer = document.getElementById('profileNameContainer');
-      // var profileName = '<h2 class="mdl-card__title-text">PROFILE_NAME</h2>';
-      var mdlGrid = document.getElementById('mdl-grid');
+      var newPost = {
+        id: 5,
+        title: newtitle.val(),
+        content: newpost.val()
+      };
 
-      for (var i = 0; i < posts.length; i++) {
-        var first_name = posts[i].first_name;
-        var last_name = posts[i].last_name;
-         $("#grid").append( `<div class="mdl-card mdl-cell mdl-cell--12-col mdl-shadow--2dp">
-             <figure class="mdl-card__media">
-               <img class="img" src="images/sky_img.jpg" alt="" width="100%" height="100%"/>
-             </figure>
-             <div class="mdl-card__title">
-               <h2 class="mdl-card__title-text">`+ first_name + " " + last_name +`</h2>
-             </div>
-                 <div class="mdl-card__supporting-text">
-                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam accusamus, consectetur.</p>
-                 </div>
-           </div>`);
-        // var setProfileName = profileName.replace("PROFILE_NAME" ,posts[i].first_name + " " + posts[i].last_name);
-        // profileNameContainer.innerHTML = setProfileName;
-      }
+      $.ajax({
+        type: 'POST',
+        url: "http://localhost:8080/infosupportAPI/services/rest/profiles/1/posts",
+        dataType: 'json',
+        data: newPost,
+        success: function(newPost){
+          console.log(newPost.content);
+        },
+        error: function(request, status, error){
+          alert(request.responseText);
+        }
+      });
+    });
 
-    }
-}
+});
