cef5c65fbaf1e7ff2e515b8d27077aac177519f1
Leonard Bos <leonard442@hotmail.com>
2017-12-05 11:39:30 +0100

    Front-end updated

--- infosupportAPI/src/main/webapp/Javascript/newsfeedJS.js
+++ infosupportAPI/src/main/webapp/Javascript/newsfeedJS.js
@@ -1,7 +1,11 @@
 $( document ).ready(function() {
 
-    let url="http://localhost:8080/infosupportAPI/services/rest/profiles";
 
+
+getPosts(); 
+    });
+$(function getPosts(){
+    let url="http://localhost:8080/infosupportAPI/services/rest/profiles";
     var postsArr = { posts:[]};
 
     $.ajax({
@@ -9,6 +13,7 @@ $( document ).ready(function() {
       url: url,
       dataType:'json',
       success: function(data){
+          
           profile = data;
            $.each(data, function(i, profile){
                     for (var i = 0; i < profile.posts.length; i++) {
@@ -21,47 +26,103 @@ $( document ).ready(function() {
             
             for (var i = 0; i < postsArr.posts.length; i++) {
                 var commentArray = { comments:[]};
-
-                
-              $("#grid").append(`<div class="mdl-card mdl-cell mdl-cell--12-col mdl-shadow--2dp">
-                      <div class="mdl-card__title">
-                        <h2 class="mdl-card__title-text">`+ postsArr.posts[i].title +`</h2>
-                      </div>
-                          <div class="mdl-card__supporting-text">
-                            <p>`+postsArr.posts[i].content+`</p>
-                          </div>
-                           
-                          <div class="mdl-card__actions mdl-card--border">
-                            <h6>Written by: `+postsArr.posts[i].username+`</h6>
-                             <button type="submit" id="addLike" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" style="float: right;">
-                                <b>Like!</b>
-                            </button>
-                            <input class="mdl-textfield__input" type="text" id="addCommentTF">
-                            <button type="submit" id="addComment" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" style="float: right;">
-                                <b>Place Comment</b>
-                            </button>
-                          </div>
-                          <div id="comments`+i+`">
-
-                          </div>
-                    </div>`);
-               
-                  var comment = "";
+                 var comment = "";
                  $.each(postsArr.posts[i].comments, function(j){
-//                    commentArray.comments.push( postsArr.posts[i].comments[j].content); 
-//                    console.log(commentArray.comments[j].content);
+                  commentArray.comments.push( postsArr.posts[i].comments[j].content); 
+                  //console.log(commentArray.comments[j].content);
                   comment += '<p>'+postsArr.posts[i].comments[j].content+'</p>';
                 });
-                alert("naar nieuwe post");
-                document.getElementById("comments"+i).innerHTML+= comment;
+                
+                                
+                $("#postRow").append(`<!-- Post Content Column -->
+        <div class="col-lg-8 pull-right" >
+
+          <!-- Title -->
+          <h1 class="mt-4">`+postsArr.posts[i].title+`</h1>
+
+          <!-- Author -->
+          <p class="lead">
+            by
+            <a href="#">`+postsArr.posts[i].username+`</a>
+          </p>
+
+          <hr>
+
+          <!-- Date/Time -->
+          <p></p>
+
+          <hr>
+
+          <!-- Preview Image -->
+          <img class="img-fluid rounded" src="images/sample_img.jpg" height="200px;" alt="">
+
+          <hr>
+
+          <!-- Post Content -->
+          <p class="lead">`+postsArr.posts[i].content+`</p>
+          <hr>
+
+          <!-- Comments Form -->
+          <div class="card my-4">
+            <h5 class="card-header bg-dark text-white">Leave a Comment:</h5>
+            <div class="card-body">
+              <form>
+                <div class="form-group">
+                  <textarea class="form-control" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+
+          <!-- Single Comment -->
+          <div  class="media mb-4">
+            <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+            <div id="comments" class="media-body">
+              <h5 class="mt-0">Commenter Name</h5>
+
+            </div>
+          </div>
+
+          <!-- Comment with nested comments -->
+          <div class="media mb-4">
+            <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+            <div class="media-body">
+              <h5 class="mt-0">Commenter Name</h5>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+
+              <div class="media mt-4">
+                <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+                <div class="media-body">
+                  <h5 class="mt-0">Commenter Name</h5>
+                  
+                </div>
+              </div>
+
+              <div class="media mt-4">
+                <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+                <div class="media-body">
+                  <h5 class="mt-0">Commenter Name</h5>
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+        <div class="col-md-4"></div>`);
+               document.getElementById("comments").innerHTML+= comment;
             };
         },
         error: function(){
           alert('error loading messages');
         }
     });
+    });
 
-    $('#addPost').on('click', function(){
+    $('#addPost').on('click', function(e){
+        
       $.ajax({
         type: 'POST',
         url: "http://localhost:8080/infosupportAPI/services/rest/profiles/1/posts",
@@ -70,9 +131,11 @@ $( document ).ready(function() {
         data: formToJSON(),
         success: function(data, textStatus, jqXHR){
           console.log(formToJson());
+          alert('succes');
         },
         error: function(request, status, error){
           alert(request.responseText);
+
         }
       });
     });
@@ -123,11 +186,11 @@ $( document ).ready(function() {
 
 	function formToJSON() {
     return JSON.stringify({
-        "id": 7,
+        "id": 11,
 		"title": $('#newPostTitle').val(),
 		"content": $('#newPost').val(),
                 "username": 'Jordybo'
         });
         }
 
-});
+
